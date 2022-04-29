cda2cd7fef24e6c5a082a7fdf3bf5ff2db0d0a84
LFelipeSRocha <luis.rocha@iesb.edu.br>
2022-04-28 21:02:18 -0300

    Third Commit, Finishing the job
    enviando o arquivo que deve crair um site e colocar tudo direitinho

--- configura_servidorWeb.sh
+++ configura_servidorWeb.sh
@@ -1,4 +1,25 @@
-#!/bin/bash
-echo testing hello!
+#!script/bash/
 
-echo testando se essa coisa está funcionando certamente!!!!!!!!!
+apt-get update
+
+apt-get install apache2
+
+apt-get install wget
+
+wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+apt-get install unzip
+
+unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+cd 873219-690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+cp -f cv-resumido.html /var/www/html/
+
+cp -f style.css /var/www/html
+
+cd /var/www/html/
+
+rm index.html
+
+mv cv-resumido.html index.html
